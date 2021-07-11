b7b103789ca045757e41e1f44235c7045b0d3b26
Basavachetan GM <chetan@revolotech.com>
2021-07-11 12:00:32 +0530

    RealTimeTracking.js implementation completed

--- src/screens/RealTimeTrackingScreen.js
+++ src/screens/RealTimeTrackingScreen.js
@@ -1,21 +1,66 @@
 import React from "react";
-import { 
-    View,
-    Text,
-    StyleSheet
-} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <View style={{ width: 40 }}></View>
+      <View style={styles.headerTitle}>
+        <Text style={styles.headerTitleText}>Real Time Tracking</Text>
+      </View>
+      <View style={styles.headerRight}>
+        <MaterialIcons name="message" size={28} color="orange" />
+      </View>
+    </View>
+  );
+};
 
 const RealTimeTrackingScreen = () => (
-    <View style={styles.container}>
-        <Text>RealTimeTrackingScreen</Text>
+  <View style={styles.container}>
+    <Header />
+    <View style={styles.contentScreen}>
+      <Text style={styles.contentScreenText}>No More bookings yet</Text>
     </View>
-    )
+  </View>
+);
 export default RealTimeTrackingScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    marginTop: 30,
+    flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    height: 70,
+    borderWidth: 0.5,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  headerTitle: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerTitleText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  headerRight: {
+    paddingTop: 5,
+  },
+  contentScreen: {
+    backgroundColor: "#ddd",
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contentScreenText: {
+    color: '#aaa',
+    fontSize: 16,
+  }
 });
